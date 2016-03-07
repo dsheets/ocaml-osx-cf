8fd5b2090ddc8de40f4ac2bbe4bacff9eaf3072d
David Sheets <sheets@alum.mit.edu>
2016-03-07 16:48:06 +0000

    Simplify the Cf_lwt.run_thread interface
    Now returns when runloop is running with the runloop value.

--- lwt/cf_lwt.mli
+++ lwt/cf_lwt.mli
@@ -16,10 +16,11 @@
  *)
 
 module RunLoop : sig
-  val run_thread : (Cf.RunLoop.t -> unit) -> unit Lwt.t
+  val run_thread : (Cf.RunLoop.t -> unit) -> Cf.RunLoop.t Lwt.t
 
   val run_thread_in_mode :
     ?return_after_source_handled:bool -> ?seconds:float -> Cf.RunLoop.Mode.t ->
-    (Cf.RunLoop.t -> unit) -> Cf.RunLoop.RunResult.t Lwt.t
+    (Cf.RunLoop.t -> unit) ->
+    (Cf.RunLoop.RunResult.t -> unit Lwt.t) -> Cf.RunLoop.t Lwt.t
 
 end
