d5306f73d0d5aa1d1716e5a9d86b70fe70e81786
David Sheets <sheets@alum.mit.edu>
2016-03-30 12:33:13 +0100

    Fix build for OS X 10.10 and earlier as 10.11 added CFRunLoopRunResult
    Should fix #3.

--- lib/osx_cf_util.c
+++ lib/osx_cf_util.c
@@ -8,9 +8,10 @@ void osx_cf_run_loop_run() {
   caml_acquire_runtime_system();
 }
 
-CFRunLoopRunResult osx_cf_run_loop_run_in_mode
+// On OS X 10.10 and earlier, the return type was SInt32
+SInt32 osx_cf_run_loop_run_in_mode
 (CFStringRef mode, CFTimeInterval seconds, Boolean returnAfterSourceHandled) {
-  CFRunLoopRunResult r;
+  SInt32 r;
   caml_release_runtime_system();
   r = CFRunLoopRunInMode(mode, seconds, returnAfterSourceHandled);
   caml_acquire_runtime_system();
--- lib/osx_cf_util.h
+++ lib/osx_cf_util.h
@@ -1,4 +1,4 @@
 void osx_cf_run_loop_run();
 
-CFRunLoopRunResult osx_cf_run_loop_run_in_mode
+SInt32 osx_cf_run_loop_run_in_mode
 (CFStringRef mode, CFTimeInterval seconds, Boolean returnAfterSourceHandled);
